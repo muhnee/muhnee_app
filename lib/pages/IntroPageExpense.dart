b9578eaa4122f61b772ae9cb659d13462867121c
nikhil <nikhil_r@live.com>
2019-12-08 20:20:51 +1100

    Grid view added

--- lib/pages/IntroPageExpense.dart
+++ lib/pages/IntroPageExpense.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:muhnee_app/routing/FadeRoute.dart';
 import '../utilities/ShowUp.dart';
+import '../utilities/SignIn.dart';
+
 
 class IntroPageExpense extends StatefulWidget {
   @override
@@ -21,14 +24,25 @@ class _IntroPageExpenseState extends State<IntroPageExpense> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           _showUpText(),
-          _choiceCells(),
+
+          Expanded(
+          
+            child: SizedBox(
+              width: 350,
+              
+              child: _expensesCells(),
+            ),
+            
+        
+        ),
+
           ShowUp(
             child: Padding(
-                padding: EdgeInsets.only(bottom: 30.0),
+                padding: EdgeInsets.only(bottom: 142.0),
                 child: Center(
                   child: _nextButton(),
                 )),
-            delay: delayAmount * 9,
+            delay: delayAmount * 6,
           ),
         ],
       ),
@@ -43,20 +57,21 @@ class _IntroPageExpenseState extends State<IntroPageExpense> {
         children: <Widget>[
           ShowUp(
             child: Padding(
-              padding: EdgeInsets.only(bottom: 33.0),
+              padding: EdgeInsets.only(bottom: 20.0),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text("Firstly, ",
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      )),
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  )),
                   Text("let's add some",
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        color: Colors.black,
-                      )),
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    color: Colors.black,
+                  )),
                 ],
               ),
             ),
@@ -64,7 +79,7 @@ class _IntroPageExpenseState extends State<IntroPageExpense> {
           ),
           ShowUp(
             child: Padding(
-              padding: EdgeInsets.only(bottom: 33.0),
+              padding: EdgeInsets.only(bottom: 20.0),
               child: Text("categories to group",
                   style: TextStyle(
                     fontSize: 30.0,
@@ -72,11 +87,11 @@ class _IntroPageExpenseState extends State<IntroPageExpense> {
                     color: Colors.black,
                   )),
             ),
-            delay: delayAmount * 3,
+            delay: delayAmount * 2,
           ),
           ShowUp(
             child: Padding(
-              padding: EdgeInsets.only(bottom: 33.0),
+              padding: EdgeInsets.only(bottom: 15.0),
               child: Text("your expenses.",
                   textAlign: TextAlign.center,
                   style: TextStyle(
@@ -86,8 +101,9 @@ class _IntroPageExpenseState extends State<IntroPageExpense> {
                     color: Colors.black,
                   )),
             ),
-            delay: delayAmount * 6,
+            delay: delayAmount * 4,
           ),
+       
         ],
       ),
     );
@@ -95,11 +111,13 @@ class _IntroPageExpenseState extends State<IntroPageExpense> {
 
   Widget _nextButton() {
     return SizedBox(
-      width: 300,
+      width: 150,
       child: GestureDetector(
         onTap: () {
-          // go to the next page
-          //Navigator.pushReplacement(context, FadeRouteBuilder(page: IntroPageIncome()));
+        
+            // Navigator.pushReplacement(
+            //     context, FadeRouteBuilder(page: IntroPageIncome()));
+     
         },
         child: Container(
           decoration: BoxDecoration(
@@ -112,7 +130,7 @@ class _IntroPageExpenseState extends State<IntroPageExpense> {
                 ),
               ]),
           child: Padding(
-            padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+            padding: EdgeInsets.only(top: 15.0, bottom: 0.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -129,15 +147,60 @@ class _IntroPageExpenseState extends State<IntroPageExpense> {
   }
 }
 
-class _choiceCells extends StatelessWidget {
+
+
+
+class _expensesCells extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var functions = ["Food", "Clothing", "Rent", "Fuel", "Tech", "Drinks", "Coffee", "Activities", "Alcohol",];
+
     return GridView.count(
-        crossAxisCount: 2,
-        children: <Widget>[
-          Text("hello"), 
-          Text("hello"), 
-        ],
-        );
+                crossAxisCount: 3,
+                //padding: EdgeInsets.symmetric(horizontal: 50.0),
+                
+                 childAspectRatio: 1 / 1,
+                 mainAxisSpacing: 0.0,
+                 //crossAxisSpacing: 0.0,
+                children: <Widget>[
+                  //for(String i in functions) Cards(title:i, page: SingleFunction(), curve: 15.0,),
+
+                  for (String i in functions) _eachCell(expenseType: i,)
+
+            ],
+          );
+  }
+}
+
+class _eachCell extends StatelessWidget {
+  var expenseType; 
+  _eachCell({this.expenseType});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+       child: SizedBox(
+         width: 91,
+         height: 35,
+        child: Container(
+          
+          child: Center(
+            child: Text(expenseType, 
+            style: TextStyle(
+              fontSize: 15.0
+            ),
+          textAlign: TextAlign.center,),
+          ), 
+
+          decoration: BoxDecoration(
+            color: Color(0xffDEDEDE),
+            borderRadius: BorderRadius.circular(50.0),
+          ),
+          
+          
+        ),
+        
+      ),
+    );
   }
 }
