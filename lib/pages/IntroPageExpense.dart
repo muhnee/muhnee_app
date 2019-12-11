7b46d21297be5841128f4cc11ed644efc9df5a36
nikhil <nikhil_r@live.com>
2019-12-11 15:46:25 +1100

    updates

--- lib/pages/IntroPageExpense.dart
+++ lib/pages/IntroPageExpense.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:muhnee_app/routing/FadeRoute.dart';
 import '../utilities/ShowUp.dart';
 import '../utilities/SignIn.dart';
+import 'IntroPageIncome.dart';
 
 class IntroPageExpense extends StatefulWidget {
   @override
@@ -33,14 +34,15 @@ class _IntroPageExpenseState extends State<IntroPageExpense> {
 
           _expCells(),
 
-          ShowUp(
-            child: Padding(
-                padding: EdgeInsets.only(bottom: 142.0),
-                child: Center(
-                  child: _nextButton(),
-                )),
-            delay: delayAmount * 6,
-          ),
+          // ShowUp(
+          // child:
+          Padding(
+              padding: EdgeInsets.only(bottom: 142.0),
+              child: Center(
+                child: _nextButton(),
+              )),
+          //   delay: delayAmount * 6,
+          // ),
         ],
       ),
     );
@@ -52,54 +54,57 @@ class _IntroPageExpenseState extends State<IntroPageExpense> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          ShowUp(
-            child: Padding(
-              padding: EdgeInsets.only(bottom: 15.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text("Firstly, ",
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      )),
-                  Text("let's add some",
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        color: Colors.black,
-                      )),
-                ],
-              ),
+          // ShowUp(
+          //   child:
+          Padding(
+            padding: EdgeInsets.only(bottom: 15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text("Firstly, ",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    )),
+                Text("let's add some",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.black,
+                    )),
+              ],
             ),
-            delay: delayAmount,
           ),
-          ShowUp(
-            child: Padding(
-              padding: EdgeInsets.only(bottom: 15.0),
-              child: Text("categories to group",
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    // fontFamily: "Montserrat",
-                    color: Colors.black,
-                  )),
-            ),
-            delay: delayAmount * 2,
+          //   delay: delayAmount,
+          // ),
+          // ShowUp(
+          //   child:
+          Padding(
+            padding: EdgeInsets.only(bottom: 15.0),
+            child: Text("categories to group",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  // fontFamily: "Montserrat",
+                  color: Colors.black,
+                )),
           ),
-          ShowUp(
-            child: Padding(
-              padding: EdgeInsets.only(bottom: 15.0),
-              child: Text("your expenses.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    // height: 2.0,
-                    // fontFamily: "Montserrat",
-                    color: Colors.black,
-                  )),
-            ),
-            delay: delayAmount * 4,
+          //   delay: delayAmount * 2,
+          // ),
+          // ShowUp(
+          // child:
+          Padding(
+            padding: EdgeInsets.only(bottom: 15.0),
+            child: Text("your expenses.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  // height: 2.0,
+                  // fontFamily: "Montserrat",
+                  color: Colors.black,
+                )),
           ),
+          //   delay: delayAmount * 4,
+          // ),
         ],
       ),
     );
@@ -110,8 +115,8 @@ class _IntroPageExpenseState extends State<IntroPageExpense> {
       width: 150,
       child: GestureDetector(
         onTap: () {
-          // Navigator.pushReplacement(
-          //     context, FadeRouteBuilder(page: IntroPageIncome()));
+          Navigator.pushReplacement(
+              context, FadeRouteBuilder(page: IntroPageIncome()));
         },
         child: Container(
           decoration: BoxDecoration(
@@ -124,7 +129,7 @@ class _IntroPageExpenseState extends State<IntroPageExpense> {
                 ),
               ]),
           child: Padding(
-            padding: EdgeInsets.only(top: 15.0, bottom: 0.0),
+            padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -302,10 +307,9 @@ class _expCells extends StatelessWidget {
                   child: Padding(
                     padding: EdgeInsets.all(12.0),
                     child: Image(
-                      image: AssetImage("lib/assets/images/tick.png"),),
-
-                  ), 
-
+                      image: AssetImage("lib/assets/images/tick.png"),
+                    ),
+                  ),
                   width: 40,
                   height: 40,
                   decoration: BoxDecoration(
@@ -330,26 +334,55 @@ class _expCells extends StatelessWidget {
 
 class SingleExpCell extends StatelessWidget {
   var expenseType;
+  var cellColor = Color(0xffDEDEDE);
 
   SingleExpCell({this.expenseType});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 91,
-      height: 34,
-      child: Center(
-        child: Text(
-          expenseType,
-          style: TextStyle(
-              fontSize: 15.0, color: Colors.black, fontWeight: FontWeight.w400),
-          textAlign: TextAlign.center,
+    return GestureDetector(
+      child: Container(
+        width: 91,
+        height: 34,
+        child: Center(
+          child: Text(
+            expenseType,
+            style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.black,
+                fontWeight: FontWeight.w400),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        decoration: BoxDecoration(
+          color: cellColor,
+          borderRadius: BorderRadius.circular(50.0),
         ),
       ),
-      decoration: BoxDecoration(
-        color: Color(0xffDEDEDE),
-        borderRadius: BorderRadius.circular(50.0),
-      ),
+      onTap: (){
+        itemSelected(expenseType, cellColor);
+      },
     );
   }
+}
+
+void itemSelected(expenseType, cellColor) async {
+
+  var selectedItems = [];
+
+  if (selectedItems.contains(expenseType)){
+    //set colour to grey and remove the item
+    selectedItems.remove(expenseType);
+
+    setState(() { });
+
+    cellColor = Color(0xffDEDEDE);
+    
+
+  } else {
+    selectedItems.add(expenseType); 
+
+  }
+
+
 }
---
+++ lib/pages/IntroPageIncome.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class IntroPageIncome extends StatelessWidget {
+  const IntroPageIncome({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      child: Text("this is a test"),
+    );
+
+  }
+}
