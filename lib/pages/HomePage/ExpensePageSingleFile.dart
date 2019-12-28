7894bcdacc9ef68d85965b4fcf8144c317d4a971
nikhil <nikhil_r@live.com>
2019-12-29 09:16:08 +1100

    fixed the expense keyboard click area

--- lib/pages/HomePage/ExpensePageSingleFile.dart
+++ lib/pages/HomePage/ExpensePageSingleFile.dart
@@ -6,10 +6,9 @@ import 'package:muhnee/utilities/SizeConfig.dart';
 import '../../main.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Color componentColor = Colors.grey[600];
-Color pageColor = Colors.white;
-// Color componentColor = Color(0xff00e56d);
-// Color pageColor = Color(0xfff2f7ea);
+Color componentColor = Colors.black;
+Color pageColor = Colors.grey[300];
+
 var amount = "";
 
 class ExpensePageSingleFile extends StatefulWidget {
@@ -22,21 +21,20 @@ class _ExpensePageSingleFileState extends State<ExpensePageSingleFile> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      backgroundColor: Color(0xff424044),
+      backgroundColor: pageColor,
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
-            Text(
-              amount,
-              textAlign: TextAlign.center,
-              style: TextStyle(
+            Text(amount,
+                textAlign: TextAlign.center,
+                style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.w600,
-                  color: Colors.white),
-            ),
+                  color: componentColor,
+                )),
             CustomKeyboard(),
           ],
         ),
@@ -49,7 +47,7 @@ class _ExpensePageSingleFileState extends State<ExpensePageSingleFile> {
         width: SizeConfig.blockSizeHorizontal * 80,
         height: SizeConfig.blockSizeVertical * 35,
         child: Container(
-          color: Color(0xff424044),
+          color: pageColor,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
@@ -81,17 +79,28 @@ class _ExpensePageSingleFileState extends State<ExpensePageSingleFile> {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: <Widget>[
                   GestureDetector(
-                    child: Icon(Icons.first_page, color: Colors.white),
+                    child: SizedBox(
+                      child: Icon(Icons.first_page, color: componentColor),
+                      width: SizeConfig.blockSizeHorizontal * 15,
+                    ),
                     onTap: () {
-                      print("btn works");
+                      print("delete char");
                       setState(() {
                         amount = amount.substring(0, (amount.length - 1));
                       });
                     },
                   ),
                   KeyboardCell("0"),
-                  Icon(Icons.check_circle_outline,
-                      size: 20, color: Colors.white),
+                  GestureDetector(
+                    child: SizedBox(
+                      child: Icon(Icons.check_circle_outline,
+                          size: 20, color: componentColor),
+                      width: SizeConfig.blockSizeHorizontal * 15,
+                    ),
+                    onTap: () {
+                      print("upload to firebase");
+                    },
+                  ),
                 ],
               ),
             ],
@@ -100,16 +109,20 @@ class _ExpensePageSingleFileState extends State<ExpensePageSingleFile> {
   }
 
   Widget KeyboardCell(cellValue) {
-    return Center(
-      child: InkWell(
-        child: Text(
-          cellValue,
-          textAlign: TextAlign.center,
-          style: GoogleFonts.kulimPark(
-            fontSize: 20,
-            fontWeight: FontWeight.w600,
-            textStyle: TextStyle(
-              color: Colors.white,
+    return GestureDetector(
+        child: SizedBox(
+          width: SizeConfig.blockSizeHorizontal * 15,
+          child: Center(
+            child: Text(
+              cellValue,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.kulimPark(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                textStyle: TextStyle(
+                  color: componentColor,
+                ),
+              ),
             ),
           ),
         ),
@@ -117,8 +130,6 @@ class _ExpensePageSingleFileState extends State<ExpensePageSingleFile> {
           setState(() {
             amount += cellValue;
           });
-        },
-      ),
-    );
+        });
   }
 }
