1710897de37064ecfb911cdda953e166732434e5
nikhil <nikhil_r@live.com>
2020-01-28 17:39:39 +1100

    profile page clean up

--- lib/pages/Home/ProfilePage.dart
+++ lib/pages/Home/ProfilePage.dart
@@ -4,7 +4,6 @@ import '../../utilities/FadeRoute.dart';
 import '../../main.dart';
 import '../../utilities/SignIn.dart';
 import '../../utilities/SizeConfig.dart';
-import 'ExpensePageSingleFile.dart';
 
 var profileInfo;
 
@@ -30,54 +29,11 @@ class _ProfilePageState extends State<ProfilePage> {
               shrinkWrap: true,
               scrollDirection: Axis.vertical,
               children: [
-                SignOutBtn()
-                // FutureBuilder<String>(
-                //   future:
-                //       profileInfo, // a previously-obtained Future<String> or null
-                //   builder:
-                //       (BuildContext context, AsyncSnapshot<String> snapshot) {
-                //     List<Widget> children;
+                SignOutBtn(), 
 
-                //     if (snapshot.hasData) {
-                //       children = <Widget>[
 
-                //         ClipRRect(
-                //           borderRadius: BorderRadius.circular(10.0),
-                //           child: Image.network(
-                //             snapshot.data,
-                //             width: SizeConfig.blockSizeHorizontal * 25,
-                //             height: SizeConfig.blockSizeHorizontal * 25,
-                //           ),
-                //         ),
-
-                //         // data
-                //       ];
-                //     } else if (snapshot.hasError) {
-                //       children = <Widget>[
-                //         NotificationCell(
-                //           message: "Network Error",
-                //           messageColor: Colors.red,
-                //         ),
-                //         SignOutBtn(),
-                //       ];
-                //     } else {
-                //       children = <Widget>[
-                //         NotificationCell(
-                //           message: "Loading...",
-                //           messageColor: Colors.grey,
-                //         ),
-                //         SignOutBtn(),
-                //       ];
-                //     }
-                //     return Center(
-                //       child: Column(
-                //         mainAxisAlignment: MainAxisAlignment.center,
-                //         crossAxisAlignment: CrossAxisAlignment.center,
-                //         children: children,
-                //       ),
-                //     );
-                //   },
-                // ),
+               
+               
               ]),
         ),
       ),
